3b2e7d04dab567cd370d6bf252ac4c3dd078edb7
yotiosoft <yotio@yotiosoft.com>
2024-02-25 03:22:36 +0900

    add headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,56 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>
+    lines: usize,
+    bytes: Option<Size>
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("YotioSoft <yotio@yotiosoft.com>")
+        .about("Rust head")
+
+        .get_matches();
+
+    Ok(Config {
+        files: 
+        lines: 
+        bytes: 
+    })
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val));
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3は正の整数なのでOK
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 数字でない文字列の場合はエラー
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0の場合もエラー
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
